869fcc7d5fdb436e08c20c622815ea139f2eb8eb
MD Mehedi Mostazir <plabonmostazir@gmail.com>
2023-09-05 02:28:24 +0600

    some modification in api.js file

--- src/Routes/api.js
+++ src/Routes/api.js
@@ -4,3 +4,6 @@ const router = express.Router();
 
 
 router.get("/blogcreate",blogControllers.create);
+router.get("/blogread",blogControllers.read);
+router.get("/blogdelete",blogControllers.delete);
+router.get("/blogupdate",blogControllers.update);
